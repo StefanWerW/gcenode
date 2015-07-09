f14978e2db1f9fad95e302c798392d4162b27b00
StefanW <scaw.dev@gmail.com>
2015-07-09 21:23:44 +0200

    t

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var http = require('http');
 
 var server = http.createServer(function(request, response){
   console.log('got a request');
-  console.log(request);
   response.write('hi');
   response.end();
 });
