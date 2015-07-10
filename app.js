35d7ef93f1606c2fb87c8d8cd65bb754140c2eb7
StefanW <scaw.dev@gmail.com>
2015-07-10 12:01:49 +0200

    t

--- app.js
+++ app.js
@@ -13,20 +13,24 @@ server.listen(8080);
 var io = require('socket.io').listen(server);
 // Register a callback function to run when we have an individual connection
 // This is run for each individual user that connects
+
+
 io.sockets.on('connection',
   // We are given a websocket object in our function
   function (socket) {
 
     console.log("We have a new client: " + socket.id);
 
+    socket.emit('playerShipId', socket.id);
+
     // When this user emits, client side: socket.emit('otherevent',some data);
-    socket.on('mouse',
+    socket.on('shipInfo',
       function(data) {
         // Data comes in as whatever was sent, including objects
-        console.log("Received: 'mouse' " + data.x + " " + data.y);
+        console.log("Received: 'shipInfo' from " + socket.id);
 
         // Send it to all other clients
-        socket.broadcast.emit('mouse', data);
+        socket.broadcast.emit('shipInfo', data);
 
         // This is a way to send to everyone including sender
         // io.sockets.emit('message', "this goes to everyone");
@@ -35,7 +39,8 @@ io.sockets.on('connection',
     );
 
     socket.on('disconnect', function() {
-      console.log("Client has disconnected");
+      console.log("player disconected: " + socket.id);
+      socket.broadcast.emit('destroyShip', socket.id);
     });
   }
 );
